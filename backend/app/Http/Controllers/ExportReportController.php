fbbb028051e427f46135fa95099b08f373129062
akosialbean <alvincastor30@gmail.com>
2024-02-27 09:30:40 +0800

    Adding final format for exported report

--- backend/app/Http/Controllers/ExportReportController.php
+++ backend/app/Http/Controllers/ExportReportController.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\Color;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
 
-class ExportReportController extends Controller implements FromCollection, WithHeadings
+class ExportReportController extends Controller implements FromCollection, WithHeadings, WithStyles, ShouldAutoSize, WithEvents
 {
     protected $reports;
 
@@ -38,4 +47,56 @@ class ExportReportController extends Controller implements FromCollection, WithH
             'TAT Resolve to Close',
         ];
     }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Style the first row as bold text.
+            1 => [
+                'font' => [
+                    'bold' => true,
+                    'color' => [
+                        'rgb' => 'ffffff'
+                    ]
+                ],
+                'fill' => [
+                    'fillType' => Fill::FILL_SOLID,
+                    'startColor' => ['argb' => '01579B']
+                ],
+                'alignment' => [
+                    'horizontal' => Alignment::HORIZONTAL_CENTER,
+                    'vertical' => Alignment::VERTICAL_CENTER,
+                    'wrapText' => true,
+                ],
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => Border::BORDER_THIN,
+                        'color' => ['argb' => '000000'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                $cellRange = 'A2:M1000'; // All cells
+                $event->sheet->getDelegate()->getStyle($cellRange)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+                // Apply array of styles to range of cells
+                $styleArray = [
+                    'borders' => [
+                        'allBorders' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                            'color' => ['argb' => '000000'],
+                        ],
+                    ],
+                ];
+                $event->sheet->getStyle($cellRange)->applyFromArray($styleArray);
+            },
+        ];
+    }
+
 }
