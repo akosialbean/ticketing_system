98524802ae56b75786ca6c886fb9f5d5912d7ecc
akosialbean <alvincastor30@gmail.com>
2024-04-05 13:53:49 +0800

    Updating tickets created per department chart

--- backend/app/Charts/DepartmentTicketChart.php
+++ backend/app/Charts/DepartmentTicketChart.php
@@ -22,8 +22,16 @@ class DepartmentTicketChart
         $departments = Ticket::select('departments.d_code',
             DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 01 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as jancreated"),
             DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 02 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as febcreated"),
-            DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 02 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as marcreated"),
-            DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 02 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as aprcreated"),
+            DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 03 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as marcreated"),
+            DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 04 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as aprcreated"),
+            DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 05 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as maycreated"),
+            DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 06 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as juncreated"),
+            DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 07 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as julcreated"),
+            DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 08 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as augcreated"),
+            DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 09 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as sepcreated"),
+            DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 10 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as octcreated"),
+            DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 11 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as novcreated"),
+            DB::raw("(SELECT COUNT('*') FROM tickets WHERE tickets.t_todepartment=$userDept AND MONTH(tickets.created_at) = 12 AND YEAR(tickets.created_at) = YEAR(CURRENT_DATE())) as deccreated"),
         )
         ->join('users', 'tickets.t_createdby', 'users.id')
         ->join('departments', 'users.id', 'departments.d_id')
@@ -33,7 +41,20 @@ class DepartmentTicketChart
         $chart = $this->chart->lineChart();
 
         foreach($departments as $department){
-            $chart->addData($department->d_code, [$department->jancreated, $department->febcreated, $department->marcreated, $department->aprcreated]);
+            $chart->addData($department->d_code, [
+                $department->jancreated,
+                $department->febcreated,
+                $department->marcreated,
+                $department->aprcreated,
+                $department->maycreated,
+                $department->juncreated,
+                $department->julcreated,
+                $department->augcreated,
+                $department->sepcreated,
+                $department->octcreated,
+                $department->novcreated,
+                $department->deccreated,
+            ]);
         }
 
         return $chart->setXAxis(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']);
